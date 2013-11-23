ffb6efd44bba0bb1f4c59ff7e895bad970d5ffd8
User <user@andy-VirtualBox.(none)>
2013-11-23 16:39:35 +0100

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyLearnSpace::Application.config.secret_key_base = '68cd7f0af614a76d459a797288b7659e985f8809a3d885477d6f617f4199529a2a4982668df3b6e6b2b8fe62f8f522bff338ec34c846e433c85af651e5d83795'
