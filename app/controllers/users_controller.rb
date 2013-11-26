76a10cf8c244166d1346834fce1ae3219e3323d2
User <user@andy-VirtualBox.(none)>
2013-11-26 15:07:44 +0100

    version user2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,24 +8,41 @@ class UsersController < ApplicationController
     @user = user.new
   end
   
+  def login
+    render "login.html.erb"
+  end
+  
   def create
     @user = User.create(params[:user]) #fehlt noch: Antwort, ob erfolgreich oder nicht (respond_with)
-    @user=User.all
-    render :action => :index
+    @user.save
+    redirect_to @user
   end
-  
+
+private  
   def show
-  @user=User.find(params[:id])
+    @user=User.find(params[:id])
   end
-  
+
+private  
+  def showByName 
+    @user=User.find_by_name(params[:name])
+    if @user.nil?
+      render "showEmptyUser.html.erb"
+    else
+      render "show.html.erb"
+    end
+  end
+private  
   def edit
     
   end 
-  
+
+private  
   def update
     
   end
-  
+
+private  
   def destroy
     u=User.find(params[:id])
     u.destroy
