d6cb1c9154ecef43b73b8ab3882c1dee0dc6adcf
User <user@andy-VirtualBox.(none)>
2013-11-27 14:40:14 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ MyLearnSpace::Application.routes.draw do
   resources :wikis
   get 'wiki/:title' => "wikis#showByName"
   
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
