af0598a9b1eacd5be7e23ff3ad5db1bcd5c9d091
User <user@andy-VirtualBox.(none)>
2013-12-20 12:15:46 +0100

    Lehrplan

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131220085411) do
 
   create_table "contents", force: true do |t|
@@ -61,4 +60,5 @@ ActiveRecord::Schema.define(version: 20131220085411) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
