cb5f1c66925115c2b22fdf98c71a7465f3b838a8
User <user@andy-VirtualBox.(none)>
2014-01-06 16:28:33 +0100

    Lehrplan, routes curriculum item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ActiveRecord::Base
   
   validates_presence_of :title, :message => " darf nicht leer sein."
   validates_presence_of :descriptionOfContent, :message => " darf nicht leer sein."
+  validates_exclusion_of :hours, :in => [0], :message => " darf nicht 0 sein"
   
   belongs_to :curriculum
   has_many :items
--- db/migrate/20140104153056_add_file_name.rb
+++
@@ -1,7 +0,0 @@
-class AddFileName < ActiveRecord::Migration
-  def change
-     add_column :data_files, :fileName, :string
-     add_column :data_files, :fileType, :string
-
-  end
-end
