882d0541b43438c83622bc8ef1027cd9c37afda9
SEllermeier <sabrina.ellermeier@mytum.de>
2016-01-16 16:28:50 +0100

    First generation of the classes of the model. Still full of errors

--- app/models/learning_objective.rb
+++ app/models/learning_objective.rb
@@ -3,7 +3,14 @@ class LearningObjective < ActiveRecord
   attr_accessible cognitiveDimension,
 
   #Relations between different parts of the website
-  #1:1 relation between KnowledgeElement and KnowledgeElement_Chat join table (?) with destroy dependence
+  #n:m relation between LearningObjective and Curriculum
+  has_and_belongs_to_many :curricula
+  has_and_belongs_to_many :task
 
+  #n:m relation between LearningObjective and Precondition with destroy dependency
+  has_many :preconditions, :dependent => :destroy
+
+  #1:1 relation between LearningObjective and KnowledgeElement
+  belongs_to :knowledge_element
 
 end
