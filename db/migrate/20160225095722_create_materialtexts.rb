a8525a6d50c21eeaff38671cf59f61344ee09aec
memmex <jonas@hafemann.me>
2016-02-26 14:21:12 +0100

    migrations

---
+++ db/migrate/20160225095722_create_materialtexts.rb
@@ -0,0 +1,10 @@
+class CreateMaterialtexts < ActiveRecord::Migration
+  def change
+    create_table :materialtexts do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
