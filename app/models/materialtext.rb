17eda09e92fcb31364128295ea7e3cb35dfaa726
memmex <jonas@hafemann.me>
2016-02-29 10:48:28 +0100

    Update materialtext.rb

--- app/models/materialtext.rb
+++ app/models/materialtext.rb
@@ -6,7 +6,7 @@ class Materialtext < ActiveRecord::Base
   validates :text, presence: true
   validates :user_id, presence: true
 
-  has_many :users
+  has_one :users
   has_and_belongs_to_many :knowledge_elements
 
 end
