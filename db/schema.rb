59736e2f8da4daa4d09faf0c08a311bb53266996
mariearndt <marie.arndt@outlook.de>
2016-03-09 11:20:18 +0100

    admin angelegt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309101356) do
+ActiveRecord::Schema.define(version: 20160309101903) do
+
+  create_table "admins", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "answers", force: true do |t|
     t.string   "text"
