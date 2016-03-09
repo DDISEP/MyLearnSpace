b7e5a10e2959ab7246ba582bb9779e2faece0579
mariearndt <marie.arndt@outlook.de>
2016-03-09 11:09:08 +0100

    teacher angelegt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201204209) do
+ActiveRecord::Schema.define(version: 20160309101356) do
 
   create_table "answers", force: true do |t|
     t.string   "text"
@@ -150,6 +150,11 @@ ActiveRecord::Schema.define(version: 20160201204209) do
   end
 
   add_index "subexercises", ["exercise_id"], name: "index_subexercises_on_exercise_id"
+
+  create_table "teachers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
