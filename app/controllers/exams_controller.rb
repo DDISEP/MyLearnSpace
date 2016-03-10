9f0c7236b7a84d5cf313657ea1e00b8ece3e3f2b
kgrettsep <katharina-grett@web.de>
2016-03-10 20:52:30 +0100

    Speicherung der Bearbeitung eines Exams 1

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -4,10 +4,35 @@ class ExamsController < ApplicationController
    @knowledgeElements = KnowledgeElement.all
  end
 
+ def new
+   @exam = Exam.new
+   @exam.maxPoints = params[:text]
+
+   respond_to do |format|
+     if @exam.save
+       format.html { redirect_to @exam, notice: 'Die Antwort wurde erfolgreich erstellt.' }
+       format.json { render action: 'show', status: :created, location: @exam }
+     else
+       format.html { render action: 'new' }
+       format.json { render json: @exam.errors, status: :unprocessable_entity }
+     end
+   end
+ end
+
  def show
  end
 
  def edit
+   @exam =Exam.new
+   @subexercises1 = Subexercise.where(examItem: TRUE, active: TRUE, cognitive_dimension: 1)
+   @exercises = Exercise.all
+   @knowledgeElements = KnowledgeElement.find_by_id(params[:id])
+   @subexercises2 = Subexercise.where(examItem: TRUE, active: TRUE, cognitive_dimension: 3)
+   @subexercises3 = Subexercise.where(examItem: TRUE, active: TRUE, cognitive_dimension: 6)
+
+ end
+
+ def create
    @exam =Exam.new
    @subexercises1 = Subexercise.where(examItem: TRUE, active: TRUE, cognitive_dimension: 1)
    @exercises = Exercise.all
