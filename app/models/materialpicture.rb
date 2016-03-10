f1539c457c3fd3df6cc7b9e01b5b07db456840f2
memmex <jonas@hafemann.me>
2016-03-10 15:48:41 +0100

    gif upload possible

--- app/models/materialpicture.rb
+++ app/models/materialpicture.rb
@@ -5,13 +5,13 @@ class Materialpicture < ActiveRecord::Base
   validates :title, presence: true
   validates :user_id, presence: true
 
-  has_attached_file :file, :styles => { :small => "500x500>" },
+  has_attached_file :file,# :styles => { :small => "500x500>" }, #pictures are always saved in original size
                     :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
   validates_attachment_presence :file
   validates_attachment_size :file, :less_than => 5.megabytes
-  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
   has_one :users
   has_and_belongs_to_many :knowledge_elements
