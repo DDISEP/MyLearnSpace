aa9f4f98f508d44c3c260dd2ea3bf6ff9cb50230
memmex <jonas@hafemann.me>
2016-03-11 16:04:50 +0100

    ke_id for creating questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -84,6 +84,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :text)
+      params.require(:question).permit(:title, :text, :user_name, :knowledge_element_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310233629) do
+ActiveRecord::Schema.define(version: 20160311143457) do
 
   create_table "answers", force: true do |t|
     t.text     "text"
@@ -273,6 +273,7 @@ ActiveRecord::Schema.define(version: 20160310233629) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_name"
+    t.integer  "knowledge_element_id"
   end
 
   create_table "solutions", force: true do |t|
