8668b72f543522031fb0c1991e99b8500bcef887
kgrettsep <katharina-grett@web.de>
2016-03-13 14:47:14 +0100

    Anpassung Model Progress

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -2,11 +2,10 @@ class Progress < ActiveRecord::Base
 
   attr_accessible :learner_id, :teacher_id, :knowledge_element_id, :grade, :submission1,:submission2,:submission3, :subexercise1_id, :subexercise2_id, :subexercise3_id
 
-  #validates :learner_id, presence: true
-  #validates :knowledge_element_id, presence: true
-  #validates :grade, presence: true
+  validates :learner_id, presence: true
+  validates :knowledge_element_id, presence: true
 
-  #belongs_to :user
-  #belongs_to :knowledge_element
+  belongs_to :user
+  belongs_to :knowledge_element
 
 end
