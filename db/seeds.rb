33850a16c3fb627699e88110d1ca2bff9a4cfc2d
martinaottinger <martina.ottinger@tum.de>
2016-04-07 14:27:07 +0200

    Beispieldaten für Solution und Performance; Subexercise ergänzt

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,26 +64,26 @@ Wiki.create(:title => "Föhn (Wind)", :article => "Ein warmer Luftstrom aus den 
 @pc.save!
 
 
-### Grett ###
+### Grett, ottinger ###
 #Subexercise
-Subexercise.create(:position => 1, :text => "y= 2x + 1", :solution_id =>1, :exercise_id => 1, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "y = x - 1", :solution_id => 2, :exercise_id => 1, :points => 2, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "Die Gerade verläuft durch den Punkt A(2/3) und m=2", :solution_id => 4, :exercise_id => 2, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "Die Gerade verläuft durch die Punkte A(1/6) und B(-3/1)", :solution_id => 2, :exercise_id => 2, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "y-Achsenabschnitt", :solution_id => 2, :exercise_id => 3, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1)
-Subexercise.create(:position => 1, :text => "Steigung", :solution_id => 2, :exercise_id => 3, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1)
-Subexercise.create(:position => 1, :text => "Geg.: A(0/3), B(4/6). Vorgehen: 6=m4 + t => t=6/4m => 6=m4 + 6/4m ...", :solution_id => 2, :exercise_id => 10, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5)
-Subexercise.create(:position => 1, :text => "Nenner", :solution_id => 2, :exercise_id => 4, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1)
-Subexercise.create(:position => 1, :text => "Zähler", :solution_id => 2, :exercise_id => 4, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1)
-Subexercise.create(:position => 1, :text => "1/2 + 3/4", :solution_id => 2, :exercise_id => 5, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "5/3 - 7/8", :solution_id => 2, :exercise_id => 5, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "3/5 : 4/3", :solution_id => 2, :exercise_id => 5, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "6/9", :solution_id => 2, :exercise_id => 6, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "5/10", :solution_id => 2, :exercise_id => 6, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "Rationale Zahlen", :solution_id => 2, :exercise_id => 8, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1)
-Subexercise.create(:position => 1, :text => "3/4  1/2  5/6  ", :solution_id => 2, :exercise_id => 9, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3)
-Subexercise.create(:position => 1, :text => "Für dein neues Smartphone zahlst du zunächst nur die Hälfte, muss dann aber ein Jahr lang jeden Monat 1/10 vom Kaufpreis zahlen.", :solution_id => 2, :exercise_id => 7, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5)
-Subexercise.create(:position => 1, :text => "Paula bezahlt 20% mehr für ihr Smartphone als Toni, ihr Vertrag kostet jedoch 10% weniger als Tonis. ", :solution_id => 2, :exercise_id => 7, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5)
+Subexercise.create(:position => 1, :text => "y= 2x + 1", :solution_id =>1, :exercise_id => 1, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 1)
+Subexercise.create(:position => 1, :text => "y = x - 1", :solution_id => 2, :exercise_id => 1, :points => 2, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 2)
+Subexercise.create(:position => 1, :text => "Die Gerade verläuft durch den Punkt A(2/3) und m=2", :exercise_id => 2, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 3)
+Subexercise.create(:position => 1, :text => "Die Gerade verläuft durch die Punkte A(1/6) und B(-3/1)", :exercise_id => 2, :points => 4, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 1)
+Subexercise.create(:position => 1, :text => "y-Achsenabschnitt", :exercise_id => 3, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1, :user_id => 2)
+Subexercise.create(:position => 1, :text => "Steigung", :exercise_id => 3, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1, :user_id => 3)
+Subexercise.create(:position => 1, :text => "Geg.: A(0/3), B(4/6). Vorgehen: 6=m4 + t => t=6/4m => 6=m4 + 6/4m ...", :exercise_id => 10, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5, :user_id => 1)
+Subexercise.create(:position => 1, :text => "Nenner", :exercise_id => 4, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1, :user_id => 2)
+Subexercise.create(:position => 1, :text => "Zähler", :exercise_id => 4, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1, :user_id => 3)
+Subexercise.create(:position => 1, :text => "1/2 + 3/4", :exercise_id => 5, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 1)
+Subexercise.create(:position => 1, :text => "5/3 - 7/8", :exercise_id => 5, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 2)
+Subexercise.create(:position => 1, :text => "3/5 : 4/3", :exercise_id => 5, :points => 1, :moderated => FALSE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 3)
+Subexercise.create(:position => 1, :text => "6/9", :exercise_id => 6, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 1)
+Subexercise.create(:position => 1, :text => "5/10", :exercise_id => 6, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 2)
+Subexercise.create(:position => 1, :text => "Rationale Zahlen", :exercise_id => 8, :points => 1, :moderated => FALSE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 1, :user_id => 3)
+Subexercise.create(:position => 1, :text => "3/4  1/2  5/6  ", :exercise_id => 9, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 3, :user_id => 1)
+Subexercise.create(:position => 1, :text => "Für dein neues Smartphone zahlst du zunächst nur die Hälfte, muss dann aber ein Jahr lang jeden Monat 1/10 vom Kaufpreis zahlen.", :exercise_id => 7, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5, :user_id => 2)
+Subexercise.create(:position => 1, :text => "Paula bezahlt 20% mehr für ihr Smartphone als Toni, ihr Vertrag kostet jedoch 10% weniger als Tonis. ", :exercise_id => 7, :points => 1, :moderated => TRUE, :active => TRUE, :examItem => TRUE, :cognitive_dimension => 5, :user_id => 3)
 
 #Exercise
 Exercise.create(:title=> "Berechnung der Nullstelle", :description=> "Berechne die Nullstelle der folgenden linearen Funktion", :user_id => 1, :knowledge_element_id=>2, :sequence => FALSE)
@@ -101,3 +101,29 @@ Exercise.create(:title=> "Bewertung des Vorgehens", :description=> "Bewerte das 
 #Progress
 Progress.create(:learner_id =>3, :teacher_id=>1, :knowledge_element_id=>1, :grade =>3)
 Progress.create(:learner_id =>3, :teacher_id=>2, :knowledge_element_id=>2, :grade =>2)
+
+#Solution
+Solution.create(:solution => "-1/2", :description => "y = 0 => 2x + 1 = 0 | -1 => 2x = -1 | * 1/2 => x = -1/2; ", :subexercise_id => 1)
+Solution.create(:solution => "1", :description => "y = 0 => x - 1 = 0 | + 1 => x = 1", :subexercise_id => 2)
+Solution.create(:solution => "y = 2x - 1", :description => "y = mx+ t = 2 x + t, mit A(2|3): 3 = 2*2 + t | - 4=>  t = -1 => y = 2x -1 ", :subexercise_id => 3)
+Solution.create(:solution => "y = -5/4x + 19/4", :description => "m = dy / dx = (1-6)/1-(-3)) = -5/4 (2 BE), y = mx+t = -5/4*x + t; A in y einsetzen: 6 = -5/4+t => t = 19/4; y = -5/4*x + 19/4", :subexercise_id => 4)
+Solution.create(:solution => "Der Punkt, in dem eine Gerade die y-Achse schneidet", :description => "In der Geradengleichung wird der y-Achsenabschnitt üblicherweise als t bezeichnet.", :subexercise_id => 5)
+Solution.create(:solution => "Steigung beschreibt den Winkel, den die Gerade zur x-Achse hat.", :description => "In der Geradengleichung wird der y-Achsenabschnitt als m bezeichnet und als dy/dx zweiter Punkte auf der Gerade bestimmt", :subexercise_id => 6)
+Solution.create(:solution => "Falsch", :description => "m wird als Verhältnis der x- und y-Werte der gegebenen Punkte bestimmt, t durch einsetzen eines Punktes", :subexercise_id => 7)
+Solution.create(:solution => "Beschreibt die Anzahl der Anteile", :description => "Ist in der Bruchschreibeweise p/q oben, also p", :subexercise_id => 8)
+Solution.create(:solution => "Beschreibt den Anteil von 1", :description => "Ist in der Bruchschreibweise p/q unten, also q", :subexercise_id => 9)
+Solution.create(:solution => "5/4", :description => "1/2 + 3/4 = 2/4 + 3 /4 = (2+3)4 = 5/4", :subexercise_id => 10)
+Solution.create(:solution => "19/24", :description => "5/3-7/8 = 40/24 - 21/24 = (40-21)/24 = 19/24", :subexercise_id => 11)
+Solution.create(:solution => "9/20", :description => "3/5 : 4/3 = (3*3)/(5*4) = 9/20", :subexercise_id => 12)
+Solution.create(:solution => "2/3", :description => "ggT (6,9)= 3; 6:3 = 2; 9:3 = 3", :subexercise_id => 13)
+Solution.create(:solution => "1/2", :description => "ggT(5,10) = 5; 5:5 = 1; 10:5 = 2", :subexercise_id => 14)
+Solution.create(:solution => "Q = {p/q | p aus Z und q aus N\{0}", :description => "Die Menge aller Brüche mit einer ganzen Zahl im Nenner und einer natürlichen Zahl im Zähler. 0 darf dabei nicht im Zähler stehen.", :subexercise_id => 15)
+Solution.create(:solution => "1/2 < 3/4 < 5/6", :description => "kgV(2,6,4) = 12; 3/4 = 9/12 1/2 = 6/12; 5/6 = 10/12;", :subexercise_id => 16)
+Solution.create(:solution => "Schlechtes Angebot", :description => "0,5 * p + 12*1/10 * p = 5/10 * p + 12/10 * p = 22/10 * p; Man müsste mehr als doppelt so viel zahlen", :subexercise_id => 17)
+Solution.create(:solution => "Abhängig von der Vertragslaufzeit", :description => "Sobald der Vertrag eine gewisse Lauftzeit hat, ist Paulas Vertrag besser. Dieser Laufzeitwert bestimmt sich als der Schnittpunkt der Geraden der Kosten pro Monat.", :subexercise_id => 18)
+
+#Performance
+Performance.create(:achieved_points => 1, :user_id => 3, :handedin => "0,5", :subexercise_id => 14)
+Performance.create(:achieved_points => 1, :user_id => 3, :handedin => "1/2", :subexercise_id => 14)
+Performance.create(:achieved_points => 0, :user_id => 3, :handedin => "nicht kürzbar", :subexercise_id => 14)
+Performance.create(:achieved_points => 0, :user_id => 3, :handedin => "4/5", :subexercise_id => 14)
